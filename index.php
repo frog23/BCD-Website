b1f935f237e3848e8620ee05eb207f8befdfd900
Dahie <public@danielsenff.de>
2009-12-30 18:27:47 +0100

    basic html for header

--- index.php
+++ index.php
@@ -3,14 +3,13 @@
 
 <?php
   render_html_header("Behind closed doors");
-  //render_header();
+  render_header();
   ?>
 
-<div class="wrap">
-  <div class="header"></div>
-  <div class="content"></div>
-  <div class="footer"></div>
-</div>
 
     <!-- Insert your content here -->
+    
+  
+
+    
 <?php render_footer(); ?>
