77954c799e06bfcb1a2e541165901b0ef61f170e
Dahie <public@danielsenff.de>
2009-12-30 19:03:45 +0100

    added demo content for index.php

--- shared/_html_header.php
+++ shared/_html_header.php
@@ -3,9 +3,12 @@
 function render_html_header($title) {
   echo '<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="de" xml:lang="de">
 <head>
-    <title>'.$title.'</title>
+	<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+  <title>'.$title.'</title>
+  <link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="swfobject.js"></script>
 </head>
 <body>';
 }
