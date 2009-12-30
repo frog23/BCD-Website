08fb5250b14ce505ecd1e6dd340d6594be8d5c88
David <frog23@gmx.de>
2009-12-30 20:47:12 +0100

    jetzt die vergessen Dateien

---
+++ trivia.php
@@ -0,0 +1,50 @@
+<?php require("shared/partials.php"); ?>
+
+
+<?php
+  render_html_header("Behind closed doors");
+  render_header();
+  ?>
+
+
+    <!-- Insert your content here -->
+<h2>Trivia</h2>
+<ul>
+<li>Auf dem kleinen Zettel steht "Hier kommst du heute nicht rein."</li>
+<li>Nachdem Delbert im Büro aufschreckt, weil er Mike erwartet und Jack fragend schaut, ist die Tür im Hintergrund geschlossen, obwohl er sie erst in der folgenden Einstellung schließt.</li>
+<li>Auf dem Überwachungsmonitor in Delberts Büro ist eine Frau zu sehen, die sich ihre Brüste fotokopiert.</li>
+<li>In der Bar sitzt Marla im Hintergrund.</li>
+<li>Das Nummernschild des Diplomatenfahrzeugs ist "0 43-1", was es bedeuted kann man sich ausrechnen. Delberts Gespräch in der Telefonzelle mussten wir 28 Mal drehen, Jacks in der Wohnung nur 2mal.</li>
+<li>Der Türspion an der Wohnungstür ist ein Bierdeckel.</li>
+<li>Der Türspion hat falsche Krümmung, weswegen Jack aussieht wie ein Schildnöck.</li>
+<li>Dosen im Gewürzregal tauschen Positionen. Die Unterführung wurde beide Male aus der gleichen Richtung gefilmt.</li>
+<li>Ja, die Brückenszene ist komplett digital, ehrlich!</li>
+<li>Delberts Versprechen war ein echter Outtake.</li>
+<li>Alle Dialoge in Jacks Wohnung wurden 2 verschiedenen Tagen gedreht, weil der Schauspieler von Jack absolut keine Stimme hatte.</li>
+<li>Jack trägt beim Zubodengehen eine Armbanduhr, niemals sonst.</li>
+<li>Taschenlampe mit der er zusammengeschlagen wird ist die selbe, die bei der ersten Begegnung mit Delbert im Hintergrund steht.</li>
+</ul>
+
+<h3>Charakternamen</h3>
+
+<li>Jack benannt nach dem Charakter Jack aus "The Shining" gespielt von Jack Nicholson.</li>
+<li>Delbert Parcher und William Grady setzen sich aus William Parcher (A Beautiful Mind) und Delbert Grady (The Shining) zusammen.</li>
+<li>Julie Merick beinhaltet einen Limerick.</li>
+<li>Pete Yäksan (gespr. Jäcksan) sollte wohl eindeutig sein.</li>
+<li>Donald Kaufman (genannter Drehbuchautor) in Anspielung auf Charlie Kaufmann in "Adaptation".</li>
+<li>Chris Challavera kam von Chris Schäfer (Akte X-Produzent) und Manny Callavera (Charakter in Grim Fandango, spanisch "Schädel")</li>
+
+<h3>Lektionen gelernt</h3>
+
+<li>Skateboards sind keine Kameradollies.</li>
+<li>Die Filmklappe hat einen Zweck, nutz sie!</li>
+<li>50% eines Drehbuchs sind Ausschuss.</li>
+<li>Die gleiche Kamera kann Resultate verschiedener Qualität bringen.</li>
+<li>Namenskonventionen sind was feines, habe viele davon!</li>
+<li>Aber, nicht mehr als eine.</li>
+
+
+  
+
+    
+<?php render_footer(); ?>
