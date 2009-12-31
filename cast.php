fdeb740a5e0e5a735b2f66e6f299d312ed420f71
David <frog23@gmx.de>
2009-12-31 02:34:28 +0100

    images cast

--- cast.php
+++ cast.php
@@ -8,6 +8,70 @@
 
 
 <h2>Cast</h2>
+	
+
+	<div class="person">
+	<h3>Ben Winter - Jack</h3>
+	<img src="images/jack.jpeg" alt="Jack" />
+	<p>Schüchtern, unsicher, allein, diese Worte beschreiben Jack. Als Zeuge eines Mordes lebt er in ständiger Angst und in Delbert findet er den einzigen Freund, der ihn versteht.</p> 
+	<p><strong>Ben Winter</strong> ist bekannt aus Filmen wie:</p>
+
+	<ul>
+		<li>Flu In The Cuckoo's Nest</li>
+		<li>Venus Attacks!</li>
+		<li>Mein Platz</li>
+	</ul>
+	</div>
+
+
+	<div class="person">
+	<h3>William Grady - Delbert Parcher</h3>
+	<img src="images/delbert.jpeg" alt="Delbert Parcher" />
+	<p>Jacks bester Freund und in der selben Firma. Sie verbringen Zeit zusammen, respektieren sich, doch Delbert hat ein Geheimnis, von dem Jack noch nichts weiß.</p> 
+	<p><strong>William Grady</strong> ist bekannt aus Filmen wie:</p>
+	<ul>
+		<li>A Beautiful Gleaming</li>
+		<li>Strange Fight</li>
+		<li>Exclamation!</li>
+	</ul>
+	</div>
+
+	<div class="person">
+	<h3>Julie Merick - Marla</h3>
+	<img src="images/marla.jpeg" alt="Marla" />
+	<p>Sie ist eine Nachbarin von Jack und fasziniert von seiner scheuen Art. Sie will herausfinden, wer hinter diesem seltsamen Menschen ist.</p> 
+	<p><strong>Julie Merick</strong> bekannt aus dem Film:</p>
+	<ul>
+		<li>Wege nach Nirgendwo</li>
+	</ul>
+	</div>
+
+
+<h2>Crew</h2>
+
+	<div class="person">
+	<h3>Pete Yäksan</h3>
+	<img src="images/pete.jpeg" alt="Pete Yäksan" />
+	<p>Wieder liefert der preisgekrönte Regisseur, der sich bereits für viele andere Meisterwerke verantwortlich zeigte, einen atemberaubenden Thriller ab.</p> 
+	<p>Weitere Filme von Pete Yäksan:</p>
+	<ul>
+		<li>Strange Fight</li>
+		<li>Mars Attacks</li>
+	</ul>
+	</div>
+	
+	<div class="person">
+	<h3>Chris Callavera</h3>
+	<img src="images/cris.jpeg" alt="Chris Calavera" />
+	<p>Der visionäre Art-Direktor, der dem Film einen ganz eigenen Look gab.</p> 
+	<p>Weitere Filme von Chris Callavera:</p>
+	<ul>
+		<li>Dance this Last Fandango</li>
+		<li>Die Verlorenen Akten</li>
+	</ul>
+	</div>
+
+
 
   
 
