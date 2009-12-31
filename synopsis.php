421d0059becfffe4dab33a2ad3e82e7e4d5ceef1
Dahie <public@danielsenff.de>
2009-12-31 03:29:45 +0100

    rescaled split.jpg, styling for synopsis

--- synopsis.php
+++ synopsis.php
@@ -7,13 +7,11 @@
 
     <!-- Insert your content here -->
 <h2>Synopsis</h2>
-<p>
-Ein Mord, ein Freund, eine Wahrheit, nur welche?
-</p>
+<p>Ein Mord, ein Freund, eine Wahrheit, nur welche?</p>
 <p>
 Jack ist unzufrieden mit seiner Arbeit, mit seinem Leben und zu allem Überfluss wird er Zeuge eines Mordes. Doch keiner glaubt ihm und er sieht
 sich allein an weiter Front. 
-<img src="media/split.jpeg" alt="Split Screen mit Telefongespräch" />
+<img src="media/split.jpeg" alt="Split Screen mit Telefongespräch" class="floatleft" />
 Sein Kollege Delbert ist der einzige, der ihm glaubt. Er ist es auch, der Jack überzeugen kann, dass er hier inmitten einer Verschwörung steht. 
 Von da an überschlagen sich die Ereignisse. Jack wird verfolgt und versinkt immer tiefer im Strudel der Ereignisse. 
 Jack vertraut sich auch seiner Nachbarin Marla an. Die wiederum hält Delbert für sehr verdächtig und fängt an Nachforschungen anzustellen. Dabei
